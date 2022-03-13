728b3effa242f40208c74e85367f7eb73fb6410a
nemillo <nemillo@hotmail.com>
2022-03-13 15:41:06 +0100

    Pure C++

--- glfw2lunar.cpp
+++ glfw2lunar.cpp
@@ -1,79 +1,44 @@
-
 #include <GL/glew.h>		// include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
-#include <assert.h>
-#include <stdarg.h> // for doing gl_log() functions that work like printf()
-#include <stdio.h>
 #include <iostream>
-#include <time.h>
-#include <stdbool.h> 
+#include <fstream>
+#include <chrono>
+#include <string>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <chrono>
+
 
 #define GL_LOG_FILE "gl.log"
+std::ofstream log_file;
 
-// global to use in timer code later
-static double previous_seconds;
+std::ostream& operator<<(std::ostream& stream, const std::chrono::system_clock::time_point& point)
+{
+    auto time = std::chrono::system_clock::to_time_t(point);
+    std::tm* tm = std::localtime(&time);
+    char buffer[26];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S.", tm);
+    stream << buffer;
+    auto duration = point.time_since_epoch();
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
+    auto remaining = std::chrono::duration_cast<std::chrono::nanoseconds>(duration - seconds);
+    // remove microsecond cast line if you would like a higher resolution sub second time representation, then just stream remaining.count()
+    auto micro = std::chrono::duration_cast<std::chrono::microseconds>(remaining);
+    return stream << micro.count();
+}
 
 /* start a new log file. put the time and date at the top */
 bool restart_gl_log() {
-	time_t now;
-	char *date;
-	FILE *file = fopen( GL_LOG_FILE, "w" );
-
-	if ( !file ) {
-		fprintf( stderr, "ERROR: could not open GL_LOG_FILE log file %s for writing\n",
-						 GL_LOG_FILE );
-		return false;
-	}
-	now = time( NULL );
-	date = ctime( &now );
-	fprintf( file, "GL_LOG_FILE log. local time %s", date );
-	fprintf( file, "build version: %s %s\n\n", __DATE__, __TIME__ );
-	fclose( file );
-	return true;
-}
-
-/* add a message to the log file. arguments work the same way as printf() */
-bool gl_log( const char *message, ... ) {
-	va_list argptr;
-	FILE *file = fopen( GL_LOG_FILE, "a" );
-	if ( !file ) {
-		fprintf( stderr, "ERROR: could not open GL_LOG_FILE %s file for appending\n",
-						 GL_LOG_FILE );
-		return false;
-	}
-	va_start( argptr, message );
-	vfprintf( file, message, argptr );
-	va_end( argptr );
-	fclose( file );
-	return true;
-}
-
-/* same as gl_log except also prints to stderr */
-bool gl_log_err( const char *message, ... ) {
-	va_list argptr;
-	FILE *file = fopen( GL_LOG_FILE, "a" );
-	if ( !file ) {
-		fprintf( stderr, "ERROR: could not open GL_LOG_FILE %s file for appending\n",
-						 GL_LOG_FILE );
-		return false;
-	}
-	va_start( argptr, message );
-	vfprintf( file, message, argptr );
-	va_end( argptr );
-	va_start( argptr, message );
-	vfprintf( stderr, message, argptr );
-	va_end( argptr );
-	fclose( file );
+	log_file.open(GL_LOG_FILE);
+	auto now = std::chrono::high_resolution_clock::now();
+    log_file << GL_LOG_FILE << " Local time : " << now << std::endl;
+	log_file.close();
 	return true;
 }
 
 /* we will tell GLFW to run this function whenever it finds an error */
 void glfw_error_callback( int error, const char *description ) {
-	gl_log_err( "GLFW ERROR: code %i msg: %s\n", error, description );
+	
 }
 
 // keep track of window size for things like the viewport and the mouse cursor
@@ -82,84 +47,12 @@ float g_gl_height = 800.0;
 
 /* we will tell GLFW to run this function whenever the framebuffer size is changed */
 void glfw_framebuffer_size_callback( GLFWwindow *window, int width, int height ) {
-	g_gl_width = (float)width;
-	g_gl_height = (float)height;
-	printf( "width %i height %i\n", width, height );
+	g_gl_width = float(width);
+	g_gl_height = float(height);
 	/* update any perspective matrices used here */
 }
 
-/* we can use a function like this to print some GL capabilities of our adapter
-to the log file. handy if we want to debug problems on other people's computers
-*/
-void log_gl_params() {
-	GLenum params[] = {
-		GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS,
-		GL_MAX_CUBE_MAP_TEXTURE_SIZE,
-		GL_MAX_DRAW_BUFFERS,
-		GL_MAX_FRAGMENT_UNIFORM_COMPONENTS,
-		GL_MAX_TEXTURE_IMAGE_UNITS,
-		GL_MAX_TEXTURE_SIZE,
-		GL_MAX_VARYING_FLOATS,
-		GL_MAX_VERTEX_ATTRIBS,
-		GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS,
-		GL_MAX_VERTEX_UNIFORM_COMPONENTS,
-		GL_MAX_VIEWPORT_DIMS,
-		GL_STEREO,
-	};
-	const char *names[] = {
-		"GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS",
-		"GL_MAX_CUBE_MAP_TEXTURE_SIZE",
-		"GL_MAX_DRAW_BUFFERS",
-		"GL_MAX_FRAGMENT_UNIFORM_COMPONENTS",
-		"GL_MAX_TEXTURE_IMAGE_UNITS",
-		"GL_MAX_TEXTURE_SIZE",
-		"GL_MAX_VARYING_FLOATS",
-		"GL_MAX_VERTEX_ATTRIBS",
-		"GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS",
-		"GL_MAX_VERTEX_UNIFORM_COMPONENTS",
-		"GL_MAX_VIEWPORT_DIMS",
-		"GL_STEREO",
-	};
-	gl_log( "GL Context Params:\n" );
-	// integers - only works if the order is 0-10 integer return types
-	for ( int i = 0; i < 10; i++ ) {
-		int v = 0;
-		glGetIntegerv( params[i], &v );
-		gl_log( "%s %i\n", names[i], v );
-	}
-	// others
-	int v[2];
-	v[0] = v[1] = 0;
-	glGetIntegerv( params[10], v );
-	gl_log( "%s %i %i\n", names[10], v[0], v[1] );
-	unsigned char s = 0;
-	glGetBooleanv( params[11], &s );
-	gl_log( "%s %i\n", names[11], (unsigned int)s );
-	gl_log( "-----------------------------\n" );
-}
-
-/* we will use this function to update the window title with a frame rate */
-void _update_fps_counter( GLFWwindow *window ) {
-	char tmp[128];
-
-	static int frame_count;
-
-	double current_seconds = glfwGetTime();
-	double elapsed_seconds = current_seconds - previous_seconds;
-	if ( elapsed_seconds > 0.25 ) {
-		previous_seconds = current_seconds;
-
-		double fps = (double)frame_count / elapsed_seconds;
-		sprintf( tmp, "opengl @ fps: %.2f", fps );
-		glfwSetWindowTitle( window, tmp );
-		frame_count = 0;
-	}
-	frame_count++;
-}
-
 int main() {
-	const float PI = acos(-1);
-	auto t_start = std::chrono::high_resolution_clock::now();
 	GLFWwindow *window;
 	const GLubyte *renderer;
 	const GLubyte *version;
@@ -167,18 +60,23 @@ int main() {
 	int sectorCount = 10;
 	int stackCount = 10;
 	float radius = 1.0f;
+	float fps = 0.0f;
+	float frame_time_cummulated = 0.0f;
 
+	restart_gl_log();
+	auto t_start = std::chrono::high_resolution_clock::now();
+	
+	log_file.open(GL_LOG_FILE,std::ios::app);
+	log_file << "t_start: " << t_start << std::endl;
+	log_file.close();
+	
 	for(int i = 0; i <= stackCount; ++i)
 	{
     	for(int j = 0; j <= sectorCount; ++j)
    		{
-        	vertices[3*sectorCount*i+3*j] = radius * cosf(-PI/2+PI/stackCount * j) * cosf(2*PI/sectorCount * i);             // r * cos(u) * cos(v)
-        	vertices[3*sectorCount*i+3*j+1] = radius * cosf(-PI/2+PI/stackCount * j) * sinf(2*PI/sectorCount * i);             // r * cos(u) * sin(v)
-			vertices[3*sectorCount*i+3*j+2] = radius*sinf(-PI/2+PI/stackCount*j); //r* sin (u)
-			
-        	printf( "vertices[%d] = %f\n", 3*sectorCount*i+3*j, vertices[3*sectorCount*i+3*j]);
-			printf( "vertices[%d] = %f\n", 3*sectorCount*i+3*j+1, vertices[3*sectorCount*i+3*j+1]);
-			printf( "vertices[%d] = %f\n", 3*sectorCount*i+3*j+2, vertices[3*sectorCount*i+3*j+2]);
+        	vertices[3*sectorCount*i+3*j] = radius * cosf(-glm::pi<float>()/2+glm::pi<float>()/stackCount * j) * cosf(2*glm::pi<float>()/sectorCount * i);             // r * cos(u) * cos(v)
+        	vertices[3*sectorCount*i+3*j+1] = radius * cosf(-glm::pi<float>()/2+glm::pi<float>()/stackCount * j) * sinf(2*glm::pi<float>()/sectorCount * i);             // r * cos(u) * sin(v)
+			vertices[3*sectorCount*i+3*j+2] = radius*sinf(-glm::pi<float>()/2+glm::pi<float>()/stackCount*j); //r* sin (u)
 		}
 	}	
 	/*vertices[0] = 0.0f;
@@ -209,13 +107,9 @@ int main() {
 		"}";
 	GLuint shader_programme, vs, fs;
 
-	restart_gl_log();
 	// start GL context and O/S window using the GLFW helper library
-	gl_log( "starting GLFW\n%s\n", glfwGetVersionString() );
-	// register the error call-back function that we wrote, above
 	glfwSetErrorCallback( glfw_error_callback );
 	if ( !glfwInit() ) {
-		fprintf( stderr, "ERROR: could not start GLFW3\n" );
 		return 1;
 	}
 
@@ -233,7 +127,6 @@ int main() {
 
 	window = glfwCreateWindow( g_gl_width, g_gl_height, "Extended Init", NULL, NULL );
 	if ( !window ) {
-		fprintf( stderr, "ERROR: could not open window with GLFW3\n" );
 		glfwTerminate();
 		return 1;
 	}
@@ -248,10 +141,7 @@ int main() {
 	// get version info
 	renderer = glGetString( GL_RENDERER ); // get renderer string
 	version = glGetString( GL_VERSION );	 // version as a string
-	printf( "Renderer: %s\n", renderer );
-	printf( "OpenGL version supported %s\n", version );
-	gl_log( "renderer: %s\nversion: %s\n", renderer, version );
-	log_gl_params();
+	
 	// tell GL to only draw onto a pixel if the shape is closer to the viewer
 	glEnable( GL_DEPTH_TEST ); // enable depth-testing
 	glEnable(GL_PROGRAM_POINT_SIZE);
@@ -296,15 +186,15 @@ int main() {
     GLint uniProj = glGetUniformLocation(shader_programme, "proj");
     glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
 
-	previous_seconds = glfwGetTime();
+	
 	while ( !glfwWindowShouldClose( window ) ) {
-		_update_fps_counter( window );
+		auto t_now = std::chrono::high_resolution_clock::now();
+        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
 		// wipe the drawing surface clear
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 		glViewport( 0, 0, g_gl_width, g_gl_height );
 
-		auto t_now = std::chrono::high_resolution_clock::now();
-        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+		
 
 		glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(
@@ -313,10 +203,10 @@ int main() {
             glm::vec3(0.0f, 0.0f, time * 0.1* 1.0f)
         );
 		
-		/*model = glm::translate(
+		model = glm::translate(
             model,
-            glm::vec3(0.0f, time * 0.1* 1.0f, 0.0f)
-        );*/
+            glm::vec3(0.0f, 0.0f, -time * 0.1* 1.0f)
+        );
         glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 		
 		// draw points 0-3 from the currently bound VAO with current in-use shader
@@ -329,7 +219,25 @@ int main() {
 		
 		// put the stuff we've been drawing onto the display
 		glfwSwapBuffers( window );
-		//printf("%f \n", time);
+		auto t_after_frame_display = std::chrono::high_resolution_clock::now();
+		float frame_time = std::chrono::duration_cast<std::chrono::duration<float>>(t_after_frame_display - t_now).count();
+		fps = 1/frame_time;
+		frame_time_cummulated += frame_time;
+
+		log_file.open(GL_LOG_FILE,std::ios::app);
+		log_file << "t_now: " << t_now << std::endl;
+		log_file << "t_after_frame_display: " << t_after_frame_display << std::endl;
+		log_file << "frame_time: " << frame_time << std::endl;
+		log_file << "fps: " << fps << std::endl;
+		log_file << "time: " << time << std::endl;
+		log_file.close();
+		if (frame_time_cummulated >= 1.0f){
+			std::string fps_title[] = {"OpenGL @ FPS: "};
+			fps_title[0].append(std::to_string(fps));
+			glfwSetWindowTitle( window,  fps_title[0].c_str() );
+			frame_time_cummulated = 0.0f;
+		}
+		
 	}
 
 	// close GL context and any other GLFW resources
